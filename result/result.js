b865597042c6e54b2fa182875115edd05629a2e4
Sunny kumar <ssingh6102000@gmail.com>
2025-07-21 11:01:40 +0530

    Sunny's cmmited

---
+++ result/result.js
@@ -0,0 +1,17 @@
+
+window.onload = () => {
+  const score = localStorage.getItem('score') || 0;
+  document.querySelector('h1.text-primary').innerText = `${score}/100`;
+
+  document.getElementById('viewSummary').addEventListener('click', function() {
+  alert("Redirecting to summary page..."); 
+   window.location.href = "/summary/summary.html"; 
+});
+
+document.getElementById('logout').addEventListener('click', function() {
+  alert("Logging out...");
+  window.location.href = "login.html"; 
+});
+
+ 
+};
